3043167e9f1fd6cb5902e88017ab46ec14a8eabb
Anamika Panna <anamika2028@gmail.com>
2020-04-09 20:15:18 -0400

    Added save button functionality

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ console.log(currentDate);
 // Day of week and month, date
 
  $("#currentDay").html(currentDate);
+
+  // # id, . class
+
+ $(".SAVE").on("click", function(){
+    console.log('SAVE');
+
+    var hour= $(this).attr("data-hour");
+    console.log(hour);
+
+    var note = $("textarea[data-hour="+hour+"]").val()
+    console.log(note);
+
+ })
+
+
+
+
